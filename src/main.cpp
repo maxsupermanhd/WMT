000db3350c1024d0148bcfec341c1874c1a24f01
maxsupermanhd <q3.max.2011@ya.ru>
2021-03-25 18:56:52 +0300

    Update license headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 /*
- * Copyright (C) Maxim Zhuchkov - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Maxim Zhuchkov <q3.max.2011@ya.ru>, May 2019
- */
+    This file is part of WMT.
+    Copyright (C) 2019-2021  Maxim Zhuchkov
+
+    WMT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    WMT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WMT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 #include "../lib/wmt.hpp"
 #include "../lib/TinyPngOut.hpp"
